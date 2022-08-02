29a70658dfbc19715d2a18f99b7e43a8c9893858
Hasnainn05 <hussnain0505@gmail.com>
2022-08-02 13:46:11 +0500

    updated

--- lib/data/repositries/populer_product_repo.dart
+++ lib/data/repositries/populer_product_repo.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:foodo/utils/app_constant.dart';
 import 'package:get/get.dart';
-
 import 'package:foodo/data/api/api_client.dart';
 
+// if application sends or get data from server class should extends getxService
 class PopulerProductRepo extends GetxService {
   final ApiClient apiClient;
   PopulerProductRepo({
